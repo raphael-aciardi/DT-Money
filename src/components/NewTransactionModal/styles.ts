385be17d97d67d9f3e7c319c4366910bb1c37561
Raphael Aciardi <109046764+raphael-aciardi@users.noreply.github.com>
2024-12-09 16:35:09 -0300

    Starting modal (styled)

---
+++ src/components/NewTransactionModal/styles.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+import * as Dialog from '@radix-ui/react-dialog'
+
+export const Overlay = styled(Dialog.Overlay)`
+    position: fixed;
+    width: 100vw;
+    height: 100vh;
+    inset: 0;
+    background-color: rgba(0, 0, 0, 0.75);
+`;
+
+export const Content = styled(Dialog.Content)`
+
+`
